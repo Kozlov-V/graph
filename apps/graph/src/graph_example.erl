094e66ee3bd2a999a3cc0e2da374e6cfab140f7f
Maxim Komar <mk@mtw.ru>
2014-03-19 17:46:18 +0400

    main grid added

--- apps/graph/src/graph_example.erl
+++ apps/graph/src/graph_example.erl
@@ -1,5 +1,10 @@
 -module(graph_example).
--export([graph1/0, graph2/0, graph3/0]).
+-export([graph1/0, graph2/0, graph3/0, graph4/0]).
+-define(SEC_PER_DAY, 86400).
+-define(SEC_PER_HOUR, 3600).
+-define(MIL, 1000000).
+-define(GD_STYLED, -2).
+-define(GD_TRANSPARENT, -6).
 
 graph1() ->
     ok = erl_ddll:load_driver("deps/elib_gd/priv/", "elib_gd_drv"),
@@ -76,6 +81,19 @@ graph3() ->
     end,
     graph(Fun, "/tmp/c.png").
 
+graph4() ->
+    Fun = fun(Gd, D) ->
+        Width = get_width(D),
+        Height = get_height(D),
+
+        Color = palette(Gd),
+        draw_rectangle(Gd, Width, Height, Color(background), Color(graphborder)),
+        draw_header(Gd, D(fontpath), D(header), Color(text), Width),
+        draw_work_period(Gd, D(shiftXleft) + 1, D(shiftY), D(sizeX) + D(shiftXleft) - 1, D(sizeY) + D(shiftY), Color(graph)),
+        draw_time_grid(Gd, D, D(gridPixels), 18537491, 1296, 1376689633, Color(highlight), Color(maingrid))
+    end,
+    graph(Fun, "/tmp/d.png").
+
 graph(Fun, FilePath) ->
     ok = erl_ddll:load_driver("deps/elib_gd/priv/", "elib_gd_drv"),
     D = default(),
@@ -101,6 +119,8 @@ default() ->
         {shiftY, 36},
         {legendOffsetY, 80},
         {header, "mylogin : simple graph"},
+        {gridPixels, 25},
+        {period, 3600},
         {fontpath, "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf"}
     ],
     fun(P) -> proplists:get_value(P, L) end.
@@ -116,7 +136,9 @@ palette({Gd, Index}) ->
         {background, {16#F0, 16#F0, 16#F0}},
         {graphborder, {16#22, 16#22, 16#22}},
         {text, {16#20, 16#20, 16#20}},
-        {graph, {16#FF, 16#FF, 16#FF}}
+        {graph, {16#FF, 16#FF, 16#FF}},
+        {highlight, {16#AA, 16#44, 16#44}},
+        {maingrid, {16#AA, 16#AA, 16#AA}}
     ],
     L = [ begin {ok, C} = gd:image_color_allocate(Gd, Index, R, G, B), {T, C} end || {T, {R, G, B}} <- Colors],
     fun(C) -> proplists:get_value(C, L) end.
@@ -137,3 +159,123 @@ draw_header({Gd, Index}, FontPath, Text, TextColor, Width) ->
 
     gd:image_string_ft(Gd, Index, TextColor, Font, 0, Xheader, Yheader, Text).
 
+-record(tg, {interval, intervalX, offset, offsetX, start, vline}).
+draw_time_grid({Gd, Index}, D, GridPixels, Period, SizeX, From, HighlightColor, MainGridColor) ->
+    % io:format("From: ~p~n", [From]),
+    FontPath = D(fontpath),
+    Intervals = [
+        {3600, 60},             % 1 min
+        {3600, 120},            % 2 min
+        {3600, 300},            % 5 min
+        {3600, 900},            % 15 min
+        {3600, 1800},           % 30 min
+        {86400, 3600},          % 1 hour
+        {86400, 10800},         % 3 hours
+        {86400, 21600},         % 6 hours
+        {86400, 43200},         % 12 hours
+        {604800, 86400},        % 1 day
+        {1209600, 604800},      % 1 week
+        {2419200, 1209600},     % 2 weeks
+        {4838400, 2419200},     % 4 weeks
+        {9676800, 4838400},     % 8 weeks
+        {19353600, 9676800}     % 16 weeks
+    ],
+    Main = fun(E) -> element(1, E) end,
+    Sub = fun(E) -> element(2, E) end,
+    
+    Raw = GridPixels * Period / SizeX,
+    % io:format("raw: ~p; '~p' '~p' '~p'~n", [Raw, GridPixels, Period, SizeX]),
+    [Interval|_] = lists:usort(fun(E1, E2) -> abs(Sub(E1) - Raw) < abs(Sub(E2) - Raw) end, Intervals),
+    
+    OffsetMinX = 12,
+    TimeGrid = fun(Int) ->
+        IntervalX = Int * SizeX / Period,
+        Offset = calculate_offset(From, Int),
+        OffsetX = Offset * SizeX / Period,
+        Start = case OffsetX < OffsetMinX of true -> 1; false -> 0 end,
+        VlineCountMax = trunc((Period - Offset) / Int),
+        VlineCount = lists:last(lists:takewhile(fun(C) -> SizeX - OffsetX - C*IntervalX >= OffsetMinX end, lists:seq(0, VlineCountMax))),
+        #tg{ interval=Int, intervalX=IntervalX, offset=Offset, offsetX=OffsetX, start=Start, vline=VlineCount }
+    end,
+
+    SubTimeGrid = TimeGrid(Sub(Interval)),
+    MainTimeGrid = TimeGrid(Main(Interval)),
+    % io:format("interval: ~p~n", [Interval]),
+    % io:format("sub: ~p~n", [SubTimeGrid]),
+    % io:format("main: ~p~n", [MainTimeGrid]),
+    
+    F = fun(N) ->
+        T = From + N * SubTimeGrid#tg.interval + SubTimeGrid#tg.offset,
+        NewPos = N * SubTimeGrid#tg.intervalX + SubTimeGrid#tg.offsetX,
+        New = T + timezone(unixtime_to_erlangtime(From)) - timezone(unixtime_to_erlangtime(T)),   % daylight saving
+        NewTime = {NewDate, {H, M, _}} = calendar:now_to_local_time(unixtime_to_erlangtime(New)),
+        DayOfWeek = calendar:day_of_the_week(NewDate),
+
+        if
+            SubTimeGrid#tg.interval < ?SEC_PER_HOUR andalso M == 0 ->
+                draw_main_period({Gd, Index}, FontPath, NewTime, NewPos, D(shiftXleft), D(sizeY), D(shiftY), HighlightColor, MainGridColor);
+            SubTimeGrid#tg.interval >= ?SEC_PER_HOUR andalso SubTimeGrid#tg.interval < ?SEC_PER_DAY andalso H == 0 ->
+                draw_main_period({Gd, Index}, FontPath, NewTime, NewPos, D(shiftXleft), D(sizeY), D(shiftY), HighlightColor, MainGridColor);
+            SubTimeGrid#tg.interval == ?SEC_PER_DAY andalso DayOfWeek == 1 -> 
+                draw_main_period({Gd, Index}, FontPath, NewTime, NewPos, D(shiftXleft), D(sizeY), D(shiftY), HighlightColor, MainGridColor);
+            SubTimeGrid#tg.interval > ?SEC_PER_DAY andalso 
+            ((N * SubTimeGrid#tg.interval) rem MainTimeGrid#tg.interval) + SubTimeGrid#tg.offset == MainTimeGrid#tg.offset ->
+                draw_main_period({Gd, Index}, FontPath, NewTime, NewPos, D(shiftXleft), D(sizeY), D(shiftY), HighlightColor, MainGridColor);
+            true ->
+                true
+        end
+    end,
+    
+    [ F(N) || N <- lists:seq(SubTimeGrid#tg.start, SubTimeGrid#tg.vline) ].
+
+unixtime_to_erlangtime(Time) -> 
+    {Time div ?MIL, Time rem ?MIL, 0}.
+
+calculate_offset(FromU, Interval) ->
+    From = unixtime_to_erlangtime(FromU),
+    case Interval > ?SEC_PER_DAY of
+        true ->
+            {FromDate, FromTime} = calendar:now_to_local_time(From),
+            Next = FromU + (7 - calendar:day_of_the_week(FromDate) rem 7) * ?SEC_PER_DAY,
+            % io:format("fromdate: ~p; offset: ~p~n", [FromDate, (7 - calendar:day_of_the_week(FromDate) rem 7) * ?SEC_PER_DAY]),
+            {NextDate, _} = calendar:now_to_local_time(unixtime_to_erlangtime(Next)),
+            % io:format("nextdate = ~p~n", [NextDate]),
+            calendar:datetime_to_gregorian_seconds({NextDate, {0,0,0}}) - calendar:datetime_to_gregorian_seconds({FromDate, FromTime});
+        false ->
+            TZ = timezone(From),
+            Interval - (FromU + TZ) rem Interval
+    end.
+
+timezone(Time) -> 
+    LocalTime = calendar:now_to_local_time(Time),
+    UTCTime = calendar:now_to_universal_time(Time),
+    calendar:datetime_to_gregorian_seconds(LocalTime) - calendar:datetime_to_gregorian_seconds(UTCTime).
+
+draw_main_period({Gd, Index}, FontPath, {{_Year, Month, Day}, {Hour, Min, _Sec}}, Pos, ShiftXleft, SizeY, ShiftY, HighlightColor, MainGridColor) ->
+    OffsetX = ShiftXleft,
+    OffsetY = SizeY + ShiftY,
+    DateArgs = case {Hour, Min} of
+        {0, 0} ->
+            ["~2..0B.~2..0B", [Day, Month]];
+        {0, _} ->
+            ["~2..0B.~2..0B ~2..0B:~2..0B", [Day, Month, Hour, Min]];
+        {_, _} ->
+            ["~2..0B:~2..0B", [Hour, Min]]
+    end,
+    Date = lists:flatten(apply(io_lib, format, DateArgs)),
+    Font = gd_font:factory(FontPath, 8),
+    {ok, W, H} = text_size({Gd, Index}, Font, Date, 3.14/2),
+    % io:format("x = '~p'; y = '~p'; pos = '~p'; date = '~s'~n", [trunc(OffsetX + Pos + W / 2), trunc(OffsetY + W + 6), Pos, Date]),
+    % io:format("OffsetY = '~p'; height = '~p';~n", [OffsetY, H]),
+    gd:image_string_ft(Gd, Index, HighlightColor, Font, 3.14/2, trunc(OffsetX + Pos + W / 2), trunc(OffsetY + H + 6), Date),
+    image_dashed_line(Gd, Index, trunc(ShiftXleft + Pos), trunc(ShiftY), trunc(ShiftXleft + Pos), trunc(SizeY + ShiftY), MainGridColor).
+
+text_size({GD, Index}, Font, String, Angle) ->
+    { ok, Bounds } = gd:image_string_ft (GD, Index, 0, Font, Angle, 0, 0, String),
+    Width = gd_bounds:max_x(Bounds) - gd_bounds:min_x(Bounds),
+    Height = gd_bounds:max_y(Bounds) - gd_bounds:min_y(Bounds),
+    { ok, Width, Height }.
+
+image_dashed_line(Gd, Index, X1, Y1, X2, Y2, Color) ->
+    gd:image_set_style(Gd, Index, [Color, Color, ?GD_TRANSPARENT, ?GD_TRANSPARENT]),
+    gd:image_line(Gd, Index, X1, Y1, X2, Y2, ?GD_STYLED).
