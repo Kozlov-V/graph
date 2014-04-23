58c72b455040d5cd0f8a0bb1c48b16083aa9de55
Maxim Komar <mk@mtw.ru>
2014-04-23 17:02:57 +0400

    handler dummy

---
+++ apps/graph/src/graph_handler.erl
@@ -0,0 +1,14 @@
+-module(graph_handler).
+-behaviour(cowboy_http_handler).
+
+-export([init/3, handle/2, terminate/3]).
+
+init({tcp, http}, Req, _Opts) ->
+    {ok, Req, undefined_state}.
+
+handle(Req, State) ->
+    {ok, Req2} = cowboy_req:reply(200, [], <<"dummy">>, Req),
+    {ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
