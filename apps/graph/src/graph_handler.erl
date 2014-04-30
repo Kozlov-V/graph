f984fbeb67b2e9e081d2ba26adbf29fe59bcc628
Maxim Komar <mk@mtw.ru>
2014-04-30 19:06:54 +0400

    added trycatch block

--- apps/graph/src/graph_handler.erl
+++ apps/graph/src/graph_handler.erl
@@ -7,6 +7,26 @@ init({tcp, http}, Req, _Opts) ->
     {ok, Req, undefined_state}.
 
 handle(Req, State) ->
+    Now = now(),
+    try input_to_output(Req) of
+
+        Bin when is_binary(Bin) ->
+            {ok, Req3} = cowboy_req:reply(200, [{<<"content-type">>, <<"image/png">>}], Bin, Req),
+            {ok, Req3, State}
+    catch
+        Exception:Reason ->
+            {_,_,Ms} = Now,
+            {{Year, Month, Day}, {Hour, Min, Sec}} = calendar:now_to_local_time(now()),
+            Filename = string:join([ integer_to_list(E) || E <- [Year, Month, Day, Hour, Min, Sec, Ms] ], "-"),
+            file:write_file("/tmp/" ++ Filename ++ ".log", term_to_binary({Exception, Reason, Req})),
+            {ok, Req3} = cowboy_req:reply(500, [], <<>>, Req),
+            {ok, Req3, State}
+    end.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
+
+input_to_output(Req) ->
     {ok, Body, Req2} = cowboy_req:body(Req),
     {ContentEncoding, Req2} = cowboy_req:header(<<"content-encoding">>, Req2),
     Content = if 
@@ -22,9 +42,5 @@ handle(Req, State) ->
     Width = proplists:get_value(<<"width">>, Decoded, 900),
     Height = proplists:get_value(<<"height">>, Decoded, 200),
     Data = [ E || {E} <- proplists:get_value(<<"data">>, Decoded) ],
-    Bin = graph:graph(graph:dim(Width, Height), graph:default_theme(), From, Period, Title, Data),
-    {ok, Req3} = cowboy_req:reply(200, [{<<"content-type">>, <<"image/png">>}], Bin, Req2),
-    {ok, Req3, State}.
-
-terminate(_Reason, _Req, _State) ->
-    ok.
+    graph:graph(graph:dim(Width, Height), graph:default_theme(), From, Period, Title, Data).
+    
